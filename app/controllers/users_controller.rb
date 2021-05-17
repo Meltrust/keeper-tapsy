c5f18936beca205675705960517e5f050971404b
meltrust <original.mtapia@outlook.com>
2021-05-16 21:57:22 -0500

    Add friendship indicators for show and index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    @user = User.find_by(params[:id])
   end
 
   def show
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,42 @@ module ApplicationHelper
       link_to('Like!', post_likes_path(post_id: post.id), method: :post)
     end
   end
+
+  def invitation_btn(user)
+    return unless current_user != user
+
+    return if current_user.friend?(user)
+
+    if current_user.pending_friends.include?(user)
+      'Invite pending'
+    else
+      link_to('Add Friend?', user_friendships_path(user_id: user.id), method: :post, class: 'profile-link')
+    end
+  end
+
+  def accept_friendship(friendship)
+    show_user = User.find_by(id: params[:id])
+    (return unless current_user == show_user)
+
+    link_to('Accept?', user_friendship_path(user_id: friendship.id), method: :put, class: 'profile-link')
+  end
+
+  def reject_friendship(friendship)
+    show_user = User.find_by(id: params[:id])
+    (return unless current_user == show_user)
+
+    link_to('Reject?', user_friendship_path(user_id: friendship.id), method: :delete, class: 'profile-link')
+  end
+
+  def friendship_flag(user)
+    "#{user.name} is already your friend" if current_user.friends.include?(user)
+  end
+
+  def friendship_flag_index(user)
+    if current_user.friends.include?(user)
+      friendship_flag(user)
+    else
+      user.name
+    end
+  end
 end
