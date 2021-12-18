3dace2c8560d23032428e8960dedf29ed8eb6714
meltrust <original.mtapia@outlook.com>
2021-12-18 14:08:05 -0600

    fix linters

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 Rails.application.routes.draw do
-
   root 'posts#index'
 
   devise_for :users
 
-  resources :users, only: [:index, :show] do
+  resources :users, only: %i[index show] do
     resources :friendships
   end
-  
-  
-  resources :posts, only: [:index, :create] do
+
+  resources :posts, only: %i[index create] do
     resources :comments, only: [:create]
-    resources :likes, only: [:create, :destroy]
+    resources :likes, only: %i[create destroy]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
