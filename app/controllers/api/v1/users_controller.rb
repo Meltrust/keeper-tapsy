daa83acab57d2e2fee7d06c7b2ac6bb561619496
Amalcxc <amalcxirsi@gmail.com>
2021-12-19 02:39:08 +0300

    users and commets controllers

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::UsersController < ApplicationController
-end
+  protect_from_forgery with: :null_session
+
+    def create
+     user = User.create(user_params)
+      if user.save
+         render json: { token: JsonWebToken.encode(sub: user.id) },     
+        status: 200
+      else
+       render json: { message: user.errors.full_messages }, status: 400
+   end
+  end
+ private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+ end
